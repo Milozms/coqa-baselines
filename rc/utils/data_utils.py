cce8d4af67df51614a6a053960d74a8817758c68
zms <zhangmaosen@pku.edu.cn>
2019-03-28 21:34:21 +0800

    disable additional_answers

--- rc/utils/data_utils.py
+++ rc/utils/data_utils.py
@@ -85,8 +85,6 @@ class CoQADataset(Dataset):
         paragraph = self.paragraphs[qas['paragraph_id']]
         question = qas['annotated_question']
         answers = [qas['answer']]
-        if 'additional_answers' in qas:
-            answers = answers + qas['additional_answers']
 
         sample = {'id': (paragraph['id'], qas['turn_id']),
                   'question': question,
