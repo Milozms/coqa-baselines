e4ec6ace523aa2b560fe2ed5104a5dd7dcc38e22
zms <zhangmaosen@pku.edu.cn>
2019-04-15 20:28:58 +0800

    fix span

--- rc/utils/data_utils.py
+++ rc/utils/data_utils.py
@@ -119,7 +119,7 @@ def get_marks_for_paragraph(qas, paragraph, config):
     result = np.zeros(len(paragraph['annotated_context']['word']), dtype=np.uint8)
     qid = qas['turn_id'] - 2     # turn_id start from 1 (Different!!!! -2 because we want previous question here)
     first_current_qid = qid - n_current + 1
-    if first_current_qid > 0:
+    if first_current_qid >= 0:
         # history questions
         for history_qas in paragraph['qas'][:first_current_qid]:
             s, e = history_qas['answer_span_start'], history_qas['answer_span_end']
